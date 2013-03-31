7e99bbbc8928bcd16a9273ef2a1777457e508a17
Tiffany Huang <tiffh@bu.edu>
2013-03-31 17:18:46 -0400

    add UI files

--- client/config.mk
+++ client/config.mk
@@ -1,15 +1,15 @@
 # Copyright (C) 2011 by Jonathan Appavoo, Boston University
-#
+
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-#
+
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,28 +18,42 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-targets = client
+UISYSTEM=$(shell uname)
 
-include ../client/config.mk
+ifeq ($(UISYSTEM),Darwin)
+  UIINCDIR = -I/opt/local/include
+  UILIBS = -L/opt/local/lib -lSDLmain -lSDL -lSDL_image -framework Cocoa
+else
+  UINCDIR = 
+  UILIBS = -lSDL
+endif
 
-client_objs = $(objs)
-libs = $(DAGAMELIB) -lpthread
+CFLAGS := -g $(UIINCDIR) -std=c99
 
-client: $(client_objs) $(DAGAMELIBARCHIVE)
-	gcc $(CFLAGS) -o $@ $(client_objs) $(libs) 
+MODULE := $(shell basename $CURDIR)
 
-## For UI
-ttymodse.o: ttymodes.c
-	gcc $(CFLAGS) ttymodes.c -c
+DAGAMELIBHDRS := types.h net.h protocol.h protocol_utils.h            \
+	protocol_session.h protocol_client.h protocol_server.h maze.h \
+	player.h ui.h game.h
+DAGAMELIBFILE := libdagame.a
+DAGAMELIBARCHIVE := ../lib/$(DAGAMELIBFILE)
+DAGAMELIB := -L../lib -ldagame
 
-tty.o: tty.c tty.h ttymodes.h
-	gcc $(CFLAGS) tty.c -c
 
-uistandalone.o: uistandalone.c uistandalone.h
-	gcc ${CFLAGS} uistandalone.c -c
+src  = $(wildcard *.c)
+objs = $(patsubst %.c,%.o,$(src))
 
-uitest.o: uitest.c uistandalone.h
-	gcc $(CFLAGS) uitest.c -c 
+ifeq ($(MODULE),lib)
+  DAGAMELIBINCS:=$(DAGAMELIBHDRS)
+else
+  DAGAMELIBINCS:=$(addprefix ../lib/,$(DAGAMELIBHDRS))
+endif
 
-uitest: uistandalone.o uitest.o tty.o ttymodes.o
-	gcc $(CFLAGS) uistandalone.o uitest.o tty.o ttymodes.o -o uitest ${UILIBS}
+
+all: $(targets)
+.PHONY: all
+
+$(objs) : $(src) $(DAGAMELIBINCS)
+
+clean:
+	rm $(objs) $(targets)
